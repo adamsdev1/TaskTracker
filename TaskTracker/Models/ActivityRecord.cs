37634be6893d033fc990b94368b2e4f198a23ebb
Tiffany Adams <tadams03@mail.roosevelt.edu>
2022-05-22 17:25:01 -0500

    Add models/db context

---
+++ TaskTracker/Models/ActivityRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TaskTracker.Models
+{
+    public partial class ActivityRecord
+    {
+        public ActivityRecord()
+        {
+            ActivityTasks = new HashSet<ActivityTask>();
+        }
+
+        public long ActivityRecordId { get; set; }
+        public long ActivityWeekId { get; set; }
+        public int UserIdNumber { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Status { get; set; }
+        public string DepartmentName { get; set; }
+        public string ManagerName { get; set; }
+        public int? SupervisorIdNumber { get; set; }
+        public string SupervisorUserName { get; set; }
+        public DateTime? InsertedDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+
+        public virtual ActivityWeek ActivityWeek { get; set; }
+        public virtual ICollection<ActivityTask> ActivityTasks { get; set; }
+    }
+}
---
+++ TaskTracker/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TaskTracker.Models
+{
+    public partial class User
+    {
+        public long UserId { get; set; }
+        public int UserIdNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string DepartmentName { get; set; }
+        public string Status { get; set; }
+    }
+}
